417250268d3614cc393b198badcbff3c7de3d1e0
farooq-teqniqly <farooq@teqniqly.com>
2024-04-06 21:06:04 -0700

    Cleanup[

--- GymManagement.Api/Program.cs
+++ GymManagement.Api/Program.cs
@@ -1,4 +1,4 @@
-namespace GymManagement.Api
+﻿namespace GymManagement.Api
 {
     public class Program
     {
@@ -6,33 +6,7 @@ namespace GymManagement.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddAuthorization();
-
-
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-
-            app.UseAuthorization();
-
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
-
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            });
 
             app.Run();
         }
