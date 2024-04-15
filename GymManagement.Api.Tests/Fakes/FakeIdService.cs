4e7501e03071a66c22b2c760c7767bb7781496a8
farooq-teqniqly <farooq@teqniqly.com>
2024-04-15 16:46:06 -0700

    Removed fakes.

--- GymManagement.Api.Tests/Fakes/FakeIdService.cs
+++
@@ -1,7 +0,0 @@
-﻿using GymManagement.Services;
-
-namespace GymManagement.Api.Tests.Fakes;
-internal class FakeIdService : IIdService
-{
-    public Guid CreateId() => new("d85fe8a0-f857-4391-a138-3479c903ba80");
-}
